922fcfbf6d3c74084e6d0138c9a0200dcb931049
vishaldevasics <vishaldevasics@gmail.com>
2024-01-14 23:35:52 +0530

    Added log Chopping problem

--- logChopping.cpp
+++ logChopping.cpp
@@ -9,17 +9,10 @@ for(int k = 0;k<n;k++){
     cin>>len;
     int a;
     int count = 0;
-    // int arr[len];
     for(int i = 0;i<len;i++){
         cin>>a;
-        if (a==2) count++;
-        if (a==3) count += 2;
-        else if(a>3 && a%2 == 0) count += a/2 + 1;
-        else if(a>4 && a%2 == 1) count += a/2 + 2;
+        count += a - 1;
     }
-    // cout<<"count = "<<count<<endl;
-    // if(count == 0) cout<<"maomao90"<<endl;
-    // else 
     if(count %2 == 1) cout<<"errorgorn"<<endl;
     else cout<<"maomao90"<<endl;
 }
